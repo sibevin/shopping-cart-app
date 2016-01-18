0a8425f5f61ba05c25c57394fdcaa5d10fb113d8
Sibevin Wang <sibevin@gmail.com>
2016-01-18 11:25:02 +0800

    A: Test the order initial status [4], [5]

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,4 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Order, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  describe "when initalize" do
+    it "should have a 'shopping' state" do
+      order = build(:order)
+      expect(order.state).to eq('shopping')
+    end
+
+    it "should have a order_number" do
+      time_now = Time.now
+      travel_to(time_now)
+      order = create(:order, created_at: time_now)
+      expect(order.order_number).to match(/#{time_now.strftime('%Y%m%d')}\d{6}/)
+    end
+  end
 end
