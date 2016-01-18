62d509c0c96fd8fd2cf3d47432bb51f37b144373
Sibevin Wang <sibevin@gmail.com>
2016-01-18 12:18:44 +0800

    C: Refine order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,18 +1,5 @@
 FactoryGirl.define do
   factory :order do
-    user nil
-order_number "MyString"
-payment_method "MyString"
-failure_reason "MyString"
-total_price "9.99"
-total_point "9.99"
-total_pay "9.99"
-created_at "2016-01-18 10:38:27"
-cancelled_at "2016-01-18 10:38:27"
-paying_at "2016-01-18 10:38:27"
-paid_at "2016-01-18 10:38:27"
-failed_at "2016-01-18 10:38:27"
-repaid_at "2016-01-18 10:38:27"
   end
 
 end
