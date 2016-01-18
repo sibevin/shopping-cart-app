7cc53b6a513a52809398c50a5427214f96113b5c
Sibevin Wang <sibevin@gmail.com>
2016-01-18 11:33:04 +0800

    A: Gen order_number when order is created [4]

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ActiveRecord::Base
+  include Uidable
+  uidable uid_name: :order_number
+
   belongs_to :user
+
+  private
+
+  def gen_order_number
+    "#{self.created_at.strftime('%Y%m%d')}#{RandomToken.gen(6, s: :n)}"
+  end
 end
