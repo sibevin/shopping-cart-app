2e70a952a5793b82212e4b9256b167a0d621dfb5
Sibevin Wang <sibevin@gmail.com>
2016-01-22 18:18:35 +0800

    A: Test and implement get_shopping_order in user model [12]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_many :orders
+
+  def get_shopping_order
+    if so = self.orders.where(state: 'shopping').take
+      return so
+    else
+      return self.orders.create(state: 'shopping')
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".get_shopping_order" do
+    let(:user) { create(:user) }
+
+    it "should return the current shopping order if it is existing" do
+      order = create(:order, user: user, state: 'shopping')
+      expect(user.get_shopping_order).to eq(order)
+    end
+
+    it "should create a new shopping order if there is no shopping order" do
+      order = user.get_shopping_order
+      shopping_orders = user.orders.where(state: 'shopping')
+      expect(shopping_orders.size).to eq(1)
+      expect(shopping_orders.take).to eq(order)
+    end
+  end
 end
